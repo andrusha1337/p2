b9b46f49ea9dd276f3ff4c9d5fa8114ecc24d1e6
Андрей <Gaidargee_andrey@mail.ru>
2017-03-04 23:27:05 +0300

    Changes №1

--- pr2.js
+++ pr2.js
@@ -1,42 +1,53 @@
 var s = 0;
 
-function Add()
-{
-    var tab = $("#Table").find("table");
-    var name = $("#Input").find("[ name = Name ]");
-    var price = $("#Input").find("[ name = Price ]");
-    if ((name.val() === "") || (price.val() === ""))
-        alert("Пожалуйста, запоните все поля!");
-    else {
-        s += Number(price.val());
-        tab.append("<tr><td onmouseover='Over(this)' onmouseout='Out(this)'>" + name.val() +
-            "</td><td onmouseover='Over(this)' onmouseout='Out(this)'>" + price.val() + "</td></tr>");
-        name.val("");
-        price.val("");
+$(".cell").hover(
+    function () {
+        console.log($(this)[0]);
+        $(this)[0].style.backgroundColor = 'white';
+    },
+    function () {
+        console.log($(this)[0]);
+        $(this)[0].style.backgroundColor = 'greenyellow';
     }
-}
-
-function Cost()
-{
-    $("#result").html("Стоимость товара: " + s);
-}
-
-function Over(cell)
-{
-    cell.style.backgroundColor = 'white';
-}
-
-function Out(cell) {
-    cell.style.backgroundColor = 'greenyellow';
-}
+)
 
 $(document).ready(function () {
     $(".imp").on("click", function () {
-        var tab = $("#Table").find("table");
+        var tab = $(".Table").find("table");
         var name = $(this);
         var price = name.next();
         s += Number(price.text());
-        tab.append("<tr><td onmouseover='Over(this)' onmouseout='Out(this)'>" + name.text() +
-            "</td><td onmouseover='Over(this)' onmouseout='Out(this)'>" + price.text() + "</td></tr>");
+        tab.append("<tr><td class='cell'>" + name.text() +
+            "</td><td class='cell'>" + price.text() + "</td></tr>");
+    });
+
+    $("#add").on("click", function () {
+        var tab = $(".Table").find("table");
+        var name = $("#Input").find("[ name = Name ]");
+        var price = $("#Input").find("[ name = Price ]");
+        if ((name.val() === "") || (price.val() === ""))
+            alert("Пожалуйста, запоните все поля!");
+        else {
+            s += Number(price.val());
+            tab.append("<tr><td class='cell'>" + name.val() +
+                "</td><td class='cell'>" + price.val() + "</td></tr>");
+            name.val("");
+            price.val("");
+        }
     })
+
+    $("#cost").on("click", function () {
+        $("#result").html("Стоимость товара: " + s);
+    })
+
+    $(".cell").hover(
+        function () {
+            console.log($(this)[0]);
+            $(this)[0].style.backgroundColor = 'white';
+        },
+        function () {
+            console.log($(this)[0]);
+            $(this)[0].style.backgroundColor = 'greenyellow';
+        }
+    )
 })
